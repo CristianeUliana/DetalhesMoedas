23bd45e205d58f108a5c54917bfa9bc789201723
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-04-14 22:44:17 -0300

    fazendo alguns valores mockados aparecerem na tela de detalhes

--- DetalhesMoedas/Classes/DetalhesMoeda.swift
+++ DetalhesMoedas/Classes/DetalhesMoeda.swift
@@ -1,44 +1,65 @@
 
 import UIKit
 
-// APAGAR DEPOIS - VIRÁ NO MÓDULO COMMONS
-struct CriptoMoeda: Codable {
-    let idIcon, assetID, name: String
-    let dataStart, dataEnd, dataQuoteStart, dataQuoteEnd: Int
-    let dataOrderbookStart, dataOrderbookEnd, dataTradeStart, dataTradeEnd: Int
-    let dataSymbolsCount, volume1HrsUsd, volume1DayUsd, volume1MthUsd: Int
+
+
+//struct MoedaElement: Codable {
+//    let assetID, name: String
+//    let typeIsCrypto: Int
+//    let dataStart, dataEnd, dataQuoteStart, dataQuoteEnd: String
+//    let dataOrderbookStart, dataOrderbookEnd, dataTradeStart, dataTradeEnd: String
+//    let dataSymbolsCount: Int
+//    let volume1HrsUsd, volume1DayUsd, volume1MthUsd, priceUsd: Double
+//    let idIcon: String
+//
+//    enum CodingKeys: String, CodingKey {
+//        case assetID = "asset_id"
+//        case name
+//        case typeIsCrypto = "type_is_crypto"
+//        case dataStart = "data_start"
+//        case dataEnd = "data_end"
+//        case dataQuoteStart = "data_quote_start"
+//        case dataQuoteEnd = "data_quote_end"
+//        case dataOrderbookStart = "data_orderbook_start"
+//        case dataOrderbookEnd = "data_orderbook_end"
+//        case dataTradeStart = "data_trade_start"
+//        case dataTradeEnd = "data_trade_end"
+//        case dataSymbolsCount = "data_symbols_count"
+//        case volume1HrsUsd = "volume_1hrs_usd"
+//        case volume1DayUsd = "volume_1day_usd"
+//        case volume1MthUsd = "volume_1mth_usd"
+//        case priceUsd = "price_usd"
+//        case idIcon = "id_icon"
+//    }
+//}
+//
+//typealias Moeda = [MoedaElement]
+
+struct MoedaElement: Codable {
+    let id, assetID, name: String
     let priceUsd: Int
+    let idIcon: String
+    let volume1HrsUsd, volume1DayUsd, volume1MthUsd: Int
 
     enum CodingKeys: String, CodingKey {
-        case idIcon = "id_icon"
+        case id
         case assetID = "asset_id"
         case name
-        case dataStart = "data_start"
-        case dataEnd = "data_end"
-        case dataQuoteStart = "data_quote_start"
-        case dataQuoteEnd = "data_quote_end"
-        case dataOrderbookStart = "data_orderbook_start"
-        case dataOrderbookEnd = "data_orderbook_end"
-        case dataTradeStart = "data_trade_start"
-        case dataTradeEnd = "data_trade_end"
-        case dataSymbolsCount = "data_symbols_count"
+        case priceUsd = "price_usd"
+        case idIcon = "id_icon"
         case volume1HrsUsd = "volume_1hrs_usd"
         case volume1DayUsd = "volume_1day_usd"
         case volume1MthUsd = "volume_1mth_usd"
-        case priceUsd = "price_usd"
     }
 }
 
-typealias CriptoMoedas = [CriptoMoeda]
-// APAGAR ATÉ AQUI
+typealias Moeda = [MoedaElement]
 
 
-
-
-public struct Botao {
-    public static let Adicionar = "ADICIONAR"
-    public static let Remover = "REMOVER"
-}
+//public struct Botao {
+//    public static let Adicionar = "ADICIONAR"
+//    public static let Remover = "REMOVER"
+//}
 
 public class DetalhesMoeda: UIView {
     
@@ -48,44 +69,50 @@ public class DetalhesMoeda: UIView {
     
     // MARK: - IBOutlets
     
-    @IBOutlet weak var siglaLabel: UILabel!
-    @IBOutlet weak var imagemFavoritos: UIImageView!
-    @IBOutlet weak var imagemMoeda: UIImageView!
+    @IBOutlet weak var siglaMoedaLabel: UILabel!
+    @IBOutlet weak var favoritoImage: UIImageView!
+    @IBOutlet weak var moedaImage: UIImageView!
     @IBOutlet weak var valorMoedaLabel: UILabel!
-    @IBOutlet weak var botaoAdcionarRemoverOutlet: UIButton!
+    @IBOutlet weak var botaoOutlet: UIButton!
     @IBOutlet weak var valorHoraLabel: UILabel!
     @IBOutlet weak var valorMesLabel: UILabel!
     @IBOutlet weak var valorAnoLabel: UILabel!
     
-   
     // MARK: - Métodos
     
-    func makeRequestDetalhes(sigla: String) {
+    public func makeRequestDetalhes(sigla: String) {
             //let newUrl = ApiRest.MoedaDetalhe.replacingOccurrences(of: "@@@", with: sigla)
-            let api = "http://rest-sandbox.coinapi.io/v1/assets/@@@?apikey=1F8A5E86-F1C9-41C7-B8BB-9DB1B81FDE7C"
-            let newUrl = api.replacingOccurrences(of: "@@@", with: sigla)
+            //let api = "https://rest-sandbox.coinapi.io/v1/assets/@@@?apikey=1F8A5E86-F1C9-41C7-B8BB-9DB1B81FDE7C"
+            //let newUrl = api.replacingOccurrences(of: "@@@", with: sigla)
+            let newUrl = "https://607797ae1ed0ae0017d6afb7.mockapi.io/api/v1/users"
             let url = URL(string: newUrl)!
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                 print(response as Any)
                 guard let responseData = data else { return }
                 do {
-                    let moeda = try JSONDecoder().decode(CriptoMoeda.self, from: responseData)
-                    self.configuraTela(moeda)
+                    let moedas = try JSONDecoder().decode(Moeda.self, from: responseData)
+                    for moeda in moedas {
+                        DispatchQueue.main.async {
+                            self.configuraTela(moeda)
+                        }
+                        
+                    }
+                    
                 } catch let error {
                     print("error: \(error)")
                 }
             }
             task.resume()
         }
-    
-    
-    func configuraTela(_ moeda: CriptoMoeda) {
-        siglaLabel.text = moeda.assetID
+
+
+    func configuraTela(_ moeda: MoedaElement) {
+        siglaMoedaLabel.text = moeda.assetID
         valorMoedaLabel.text = "$ \(moeda.priceUsd)"
         valorHoraLabel.text = "$ "
         valorMesLabel.text = "$ "
         valorAnoLabel.text = "$ "
-        
+
     }
     
     
@@ -131,7 +158,7 @@ public class DetalhesMoeda: UIView {
 
 
 extension UIView {
-    
+
     public static var bundleUI: Bundle {
         var bundle: Bundle
         if let b = Bundle(identifier: "org.cocoapods.demo.DetalhesMoedas") {
@@ -141,11 +168,11 @@ extension UIView {
         }
         return bundle
     }
-    
+
     public class func fromNib() -> Self {
         return fromNib(viewType: self)
     }
-    
+
     public class func fromNib<T: UIView>(viewType: T.Type) -> T {
         if let nib = bundleUI.loadNibNamed(
             String(describing: viewType), owner: nil, options: nil)?.first as? T {
@@ -153,7 +180,6 @@ extension UIView {
         }
         return T()
     }
-    
 }
 
 
--- Example/DetalhesMoedas/ViewController.swift
+++ Example/DetalhesMoedas/ViewController.swift
@@ -9,110 +9,19 @@
 import UIKit
 import DetalhesMoedas
 
-struct CriptoMoeda: Codable {
-    let idIcon, assetID, name: String
-    let dataStart, dataEnd, dataQuoteStart, dataQuoteEnd: Int
-    let dataOrderbookStart, dataOrderbookEnd, dataTradeStart, dataTradeEnd: Int
-    let dataSymbolsCount, volume1HrsUsd, volume1DayUsd, volume1MthUsd: Int
-    let priceUsd: Int
-
-    enum CodingKeys: String, CodingKey {
-        case idIcon = "id_icon"
-        case assetID = "asset_id"
-        case name
-        case dataStart = "data_start"
-        case dataEnd = "data_end"
-        case dataQuoteStart = "data_quote_start"
-        case dataQuoteEnd = "data_quote_end"
-        case dataOrderbookStart = "data_orderbook_start"
-        case dataOrderbookEnd = "data_orderbook_end"
-        case dataTradeStart = "data_trade_start"
-        case dataTradeEnd = "data_trade_end"
-        case dataSymbolsCount = "data_symbols_count"
-        case volume1HrsUsd = "volume_1hrs_usd"
-        case volume1DayUsd = "volume_1day_usd"
-        case volume1MthUsd = "volume_1mth_usd"
-        case priceUsd = "price_usd"
-    }
-}
-
-typealias CriptoMoedas = [CriptoMoeda]
-
 class ViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var myStack: UIStackView!
-    
-    // let controller = TesteViewController()
-    
-    var favoritos: String = ""
-    
+
     let detalhes = DetalhesMoeda.fromNib()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
-        makeRequest()
+        myStack.addSubview(detalhes)
+        detalhes.makeRequestDetalhes(sigla: "BTC")
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func setup() {
-        //let detalhes = DetalhesMoeda.fromNib()
-        //detalhes.setupUI(moedaDelegate: self)
-        myStack.addSubview(detalhes)
-    }
-
-    private func makeRequest() {
-        let url = URL( string: "https://6076e5cf1ed0ae0017d6a02f.mockapi.io/api/v1/users")!
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            print(response as Any)
-            guard let responseData = data else { return }
-            do {
-                let moedas = try JSONDecoder().decode(CriptoMoedas.self, from: responseData)
-                for moeda in moedas {
-                    self.favoritos += "\(moeda.assetID)|"
-                }
-                print(self.favoritos)
-            } catch let error {
-                print("error: \(error)")
-            }
-        }
-        task.resume()
-    }
-    
-//    func buttonAction() {
-//
-//        var listaDeFavoritos = favoritos.split(separator: "|")
-//
-//
-//
-//
-//
-//
-//
-//        print(listaDeFavoritos)
-//    }
-        
-//    @IBAction func botao(_ sender: UIButton) {
-//
-//        show(controller, sender: self)
-//
-//      //navigationController?.pushViewController(controller, animated: true)
-//    }
-    
-   
-    
 }
-
-
-//extension UIViewController: DetalhesMoedaDelegate {
-//    public func buttonAction() {
-//        print(favoritos)
-//
-//
-//    }
-//}
